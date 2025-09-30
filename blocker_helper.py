20d807f2b269042491db02ca8777fd2bffd9d3cc
Rafael-VP <rafaelvpovoa@gmail.com>
2025-09-30 19:11:27 -0300

    Removed unnecessary file

--- blocker_helper.py
+++
@@ -1,6 +0,0 @@
-# blocker_helper.pyw
-import sys
-
-# This script does nothing and exits instantly. It's used as a "debugger"
-# to intercept and block other programs from running on Windows.
-sys.exit()
